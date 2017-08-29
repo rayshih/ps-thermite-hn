9b0cb5aa0a96bb75d894458e8785a2b675193475
Ray Shih <mnf.shih@gmail.com>
2017-08-29 13:09:28 +0900

    handle component did mount callback and able to query api

--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,9 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Trans.Class (lift)
 import DOM (DOM)
 import DOM.HTML (window) as DOM
 import DOM.HTML.Types (htmlDocumentToDocument)
@@ -12,12 +14,15 @@ import DOM.Node.NonElementParentNode (getElementById)
 import DOM.Node.Types (ElementId(..), documentToNonElementParentNode)
 import Data.Array ((..))
 import Data.Maybe (Maybe(..))
+import Network.HTTP.Affjax (AJAX)
+import Network.HTTP.Affjax as Ajax
 import React (ReactElement, createFactory)
+import React as React
 import React.DOM as R
 import ReactDOM as RD
 import Thermite as T
 
-data Action
+data Action = RootDidMount
 
 type Story = { title :: String }
 
@@ -35,14 +40,35 @@ renderStoryItem { title } = R.div' [ R.text title ]
 renderStoryList :: Array Story -> Array ReactElement
 renderStoryList = map renderStoryItem
 
+performAction :: forall eff props.
+                 T.PerformAction (ajax :: AJAX, console :: CONSOLE | eff) State props Action
+performAction RootDidMount props state = do
+  result :: Ajax.AffjaxResponse String <- getTopStories
+  liftEff $ log result.response
+  where getTopStories =
+          lift $ Ajax.get "https://hacker-news.firebaseio.com/v0/topstories.json"
+
 render :: forall eff. T.Render State eff Action
 render dispatch _ state _ =
   [ R.p' [ R.text "Hello thermite!!" ]
   , R.div' $ renderStoryList state.topStories
   ]
 
-spec :: forall eff. T.Spec eff State Unit Action
-spec = T.simpleSpec T.defaultPerformAction render
+spec :: forall eff. T.Spec (ajax :: AJAX, console :: CONSOLE | eff) State Unit Action
+spec = T.simpleSpec performAction render
+
+type ReactSpec props state eff =
+  { spec :: React.ReactSpec props state eff
+  , dispatcher :: React.ReactThis props state -> Action -> T.EventHandler
+  }
+
+withComponentDidMount :: forall props state eff
+                       . ReactSpec props state eff
+                      -> ReactSpec props state eff
+withComponentDidMount rs = rs { spec = specWithMount }
+  where
+    specWithMount = rs.spec { componentDidMount = handleDidMount }
+    handleDidMount this = rs.dispatcher this RootDidMount
 
 main :: forall e. Eff (console :: CONSOLE, dom :: DOM | e) Unit
 main = do
@@ -51,7 +77,9 @@ main = do
   maybeContainer <- getElementById (ElementId "app") $ documentToNonElementParentNode document
   case maybeContainer of
     Just container -> do
-      let component = T.createClass spec initState
-      let reactElement = createFactory component unit
+      let
+        reactSpec = T.createReactSpec spec initState
+        component = React.createClass <<< _.spec $ withComponentDidMount reactSpec
+        reactElement = createFactory component unit
       void $ RD.render reactElement container
     Nothing -> log "container #app not found"
